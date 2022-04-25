d76c9d8ce4e99a83740719e55b397ce27bbfc936
Roshanitft <99382098+Roshanitft@users.noreply.github.com>
2022-04-25 17:56:05 +0530

    Update TestVPN_top10domain2.java

--- Heritage/src/test/java/testpackage/TestVPN_top10domain2.java
+++ Heritage/src/test/java/testpackage/TestVPN_top10domain2.java
@@ -44,6 +44,7 @@ public class TestVPN_top10domain2 {
         System.setProperty( "webdriver.chrome.driver", "/home/runner/work/heritageproject/heritageproject/Heritage/chromedriver");
         String extension_Path = System.getProperty("user.dir");
         ChromeOptions chromeOptions = new ChromeOptions();
+	options.addArgument("headless");
         chromeOptions.addExtensions(new File( "/home/runner/work/heritageproject/heritageproject/Heritage/extensions/Urban-Free-VPN-proxy-Unblocker---Best-VPN.crx"));
    
      	driver = new ChromeDriver(chromeOptions);
